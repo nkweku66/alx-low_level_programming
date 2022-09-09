77abd880aa21a14c3738190e8ed0c5608d6df985
nkweku66 <n.kweku66@gmail.com>
2022-09-08 23:09:22 -0700

    initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art, using the printf function.
+ * it returns 0 upon compiling
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
