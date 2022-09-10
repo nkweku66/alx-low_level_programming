2ba9c8167478f40a1e9baf6d0d7593d3a48523d2
nkweku66 <n.kweku66@gmail.com>
2022-09-09 20:19:43 -0700

    New conditional file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase, and then in uppercase, * followed by a new line
+ * main - prints alphabets in lowercase and uppercase
  * Return: Always 0 (Success)
  */
 int main(void)
