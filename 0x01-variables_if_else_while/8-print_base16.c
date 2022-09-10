c136215151cda22b4e9b5545fc8f39b89ae1610e
nkweku66 <n.kweku66@gmail.com>
2022-09-09 21:32:46 -0700

    new file on variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int n;
 	char ch;
+
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
- 	for (ch = 'a'; ch <= 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
