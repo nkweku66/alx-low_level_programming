d82c8b5ec49d7e70e54c99198d45d5495f964c87
nkweku66 <n.kweku66@gmail.com>
2022-09-14 20:58:16 -0700

    printing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,6 +7,7 @@
 void print_to_98(int n)
 {
 	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -22,7 +23,7 @@ void print_to_98(int n)
 		{
 			if (j != 98)
 				printf("%d, ", j);
-			else if (j == 98);
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}
