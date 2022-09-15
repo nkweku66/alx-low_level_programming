f6b8dbb473a20a0ab529912f907a2cd632870a6c
nkweku66 <n.kweku66@gmail.com>
2022-09-14 19:13:03 -0700

    printing

---
+++ 0x02-functions_nested_loops/2-print_alphabet_X10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10prints 10 lines of the lower cased alphabets
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	while (i < 10)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
