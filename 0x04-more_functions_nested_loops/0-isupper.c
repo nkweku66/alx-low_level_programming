a15b68ee95dee2fda9375853c44d5daf27eccd49
nkweku66 <n.kweku66@gmail.com>
2022-09-16 12:38:07 -0700

    more nested functions plus dbugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main -checks to see if a character is uppercase
+ * @c -character to be checked
+ * Return 0 indicating completion
+ */
+
+int _isupper(int c)
+{
+	return ( c >= 'A' && c <= 'Z');
+}
