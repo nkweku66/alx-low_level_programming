c107f206a404c29d664b8a7fef6d14c6c3fd4d4c
nkweku66 <n.kweku66@gmail.com>
2022-09-16 16:03:55 -0700

    nested

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	} else
 	{
 		int i, j;
@@ -17,13 +17,13 @@ void print_triangle(int size)
 		{
 			for (j = i; j < size; j++)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
 			for (j = 1; j <= i; j++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-		putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
