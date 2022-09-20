ceb71061ef21783302c888f3458949528b4588c6
nkweku66 <n.kweku66@gmail.com>
2022-09-19 22:05:08 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int- swaps the two integers
+ * @a: points to the first value
+ * @b: points to the second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	c = *a;
+	*a = *b;
+	*b = *a;
+}
