d4c94b4564c27991c9a705862a03974d73ef543d
nkweku66 <n.kweku66@gmail.com>
2022-09-20 20:52:58 -0700

    advanced task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,11 +17,15 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
+		{
 			++d;
+		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
