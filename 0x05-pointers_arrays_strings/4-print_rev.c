d431db5612e9e78688e59acbb5b46ac49f6e55dc
nkweku66 <n.kweku66@gmail.com>
2022-09-20 21:15:07 -0700

    advanced task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,21 +4,17 @@
  * print_rev - print a string in the reverse
  * @s: string to be reversed
  */
-
 void print_rev(char *s)
 {
 	int i = 0;
-
+	
 	while (*(s + i) != '\0')
-	{
 		i += 1;
-		i -= 1;
-
-		while (i >= 0)
-		{
-			_putchar(*(s + i));
-			i--;
-		}
-		_putchar('\n');
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
 	}
+	_putchar('\n');
 }
