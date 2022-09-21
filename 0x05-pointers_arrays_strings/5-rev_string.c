74a75683aac7ccab1be20fc4d3c1a12b914ac55f
nkweku66 <n.kweku66@gmail.com>
2022-09-20 21:05:55 -0700

    advanced task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,41 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * rev_string - prints a string in reverse
- * @s: sting to be reversed
- */
 
-//void rev_string(char *s)
-//{
-//	int len, count, end;
-//	char beg;
-//
-//	len = 0;
-//
-//	while (s[len] != '\0')
-//	{
-//		len++;
-//		len--;
-//		for (count = 0; count <= len / 2; count++)
-//		{
-//			beg = s[count];
-//			end = s[len - count];
-//			s[count] = end;
-//			s[len - count] = beg;
-//		}
-//	}
-//}
+/** rev_string - print a string in reverse followed by a new line
+ * @s: string to be reversed
+ */
 
 void rev_string(char *s)
 {
-	int i, len, temp;
-	len = strlen(s);
-
-	for (i = 0; i < len/2; i++)
+	int len, count, end;
+	char beg;
+	
+	len = 0;
+        while (s[len] != '\0')
+		len++;
+	len--;
+	for (count = 0; count <= len / 2; count++)
 	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		beg = s[count];
+		end = s[len - count];
+		s[count] = end;
+		s[len - count] = beg;
 	}
 }
-
