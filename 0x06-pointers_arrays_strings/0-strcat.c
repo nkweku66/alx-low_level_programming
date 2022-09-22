1c446f4ca532d7e4ab2f18ab3283a34f42e42417
nkweku66 <n.kweku66@gmail.com>
2022-09-22 15:45:11 -0700

    programming files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,22 +7,20 @@
  * Return: dest
  */
 
-char *_strcat(char *dest, char *src) 
+char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-       	{
+	{
 		i++;
 	}
-
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		j++;
 		i++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
