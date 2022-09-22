ced0bcc62d12744f3059cbe03afd4bd2aeb735bc
nkweku66 <n.kweku66@gmail.com>
2022-09-22 15:31:23 -0700

    programming files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+?
+/**
+ * _strcat - concantenates the strings
+ * @dest: contains first string
+ * @src: string to be concatenated to the dest
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src) 
+{
+	dest[];
+	src[];
+
+	strcat(src, dest);
+	return (dest);
+}
