90f6cd4a40eaae9fd913da86722645d9f952ddf3
nkweku66 <n.kweku66@gmail.com>
2022-09-26 19:52:00 -0700

    programming files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _memset: sets the memory 
+ * @s: a pointer
+ * @b: a byte
+ * @n: the number of bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, n, b);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+new pointers directory
