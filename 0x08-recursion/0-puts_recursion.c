503498db284cd26c85da3fa849d863b254c61a8a
nkweku66 <n.kweku66@gmail.com>
2022-09-28 17:36:56 -0700

    programming files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: this is the string value
+ *
+ * The function will check an assigned value if its string will print it
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar("%s \n", *s);
+	if ( (s >= 65 && s <= 90) || (s >= 97 && s <= 122))
+	{
+		_putchar("%s \n", *s);
+		return (0);
+	}
+}
