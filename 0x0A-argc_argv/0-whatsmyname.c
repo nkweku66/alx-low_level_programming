415b5c79d7745ac0b989f5de5a8a372991bf2d48
nkweku66 <n.kweku66@gmail.com>
2022-10-15 12:04:01 -0700

    programming files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main: contains the arguments
+ * @argc: is the argument count
+ * @argv: is the argument vector
+ * Returns: zero upon success
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
