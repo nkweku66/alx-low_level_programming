579e8dc10507b5d3ca8be87f68ce3ccfa4a9e3b1
nkweku66 <n.kweku66@gmail.com>
2022-10-16 09:01:33 -0700

    programming files

---
+++ 0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - contains the arguments
+ * @argc: contains the argument count
+ * @argv: contains the argument vector
+ *
+ * Return: returns zero upon success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return(0);
+}
