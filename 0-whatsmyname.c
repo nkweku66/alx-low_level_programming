c1721fff13512ac430a1726783b4efb1cb34fb39
nkweku66 <n.kweku66@gmail.com>
2022-10-16 10:45:57 -0700

    programming files

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -7,8 +7,11 @@
  * Return: returns zero upon success
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if(argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);	
 }
