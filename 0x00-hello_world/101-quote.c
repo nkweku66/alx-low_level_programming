517b58b6bda90fc9fb713e26baf039f57a5a963f
nkweku66 <nkweku66@gmail.com>
2023-09-13 01:56:29 -0700

    print using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints void
+ *
+ * Return: 1 on success
+ */
+
+int main(void)
+{
+	write(1, "and that pieces of art is useful\" - Dora Korpar, 2015-10-19", 31);
+	return (1);
