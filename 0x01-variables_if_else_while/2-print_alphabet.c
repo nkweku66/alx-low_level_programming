c1a86a2ad498a4b4d2db5ebf61dc9aeab2ab1b53
nkweku66 <nkweku66@gmail.com>
2023-09-13 19:08:59 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 upon success
+ * Author - Nana Obeng
+ */
+
+int main(void)
+{
+	for (int a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
