d51d8410b25b52e5ef30d953d2ef7f0d9b4693c4
nkweku66 <nkweku66@gmail.com>
2023-09-13 18:33:05 -0700

    check if greater or less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,14 +22,14 @@ int main(void)
         if (d > 5)
         {
                 printf("%s %d is %d and is greater than 5\n", stg, n, d);
-        }
-        else if (d < 6 && d != 0)
-        {
+	}
+	else if (d < 6 && d != 0)
+	{
 		printf("%s %d is %d and is less than 6 and not 0\n", stg, n, d);
-        }
-        else
-        {
-                printf("%s %d is %d and is 0\n", stg, n, d);
-        }
-        return (0);
+	}
+	else
+	{
+		printf("%s %d is %d and is 0\n", stg, n, d);
+	}
+	return (0);
 }
