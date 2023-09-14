076c7a367629f41bf7d5dd64ff8fbcd7f0acdcc8
nkweku66 <nkweku66@gmail.com>
2023-09-13 18:12:38 -0700

    check if greater or less than

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 on success
+ * Author - Nana Obeng
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d =Math.abs(n % 10);
+	stg = "Last digit of";
+
+        if (d > 5)
+        {
+                printf("%s %d is %d and is greater than 5\n", stg, n, d);
+        }
+        else if (d < 6 && !=0)
+        {
+		printf("%s %d is %d and is less than 6 and not 0\n", stg, n, d);
+        }
+        else
+        {
+                printf("%s %d is %d and is 0\n", stg, n, d);
+        }
+        return (0);
+}
