a2aaa65fcb46a4dccefe68c788a914b749e397d6
nkweku66 <nkweku66@gmail.com>
2023-09-14 09:09:02 -0700

    print combination of single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,16 @@ int main(void)
 	int a = 48;
 	int b = 57;
 
-	for (; a <= b; a++)
+        for (; a <= b; a++)
 	{
+		putchar(a);
+
 		if (a != 57)
 		{
-			putchar(a);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(a);
-		}
 	}
+	putchar('\n');
 	return (0);
 }
