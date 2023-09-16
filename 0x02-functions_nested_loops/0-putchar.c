ebbe03b21bb058432b23fedf00c923906413b834
nkweku66 <nkweku66@gmail.com>
2023-09-16 16:06:54 -0700

    print putchar to stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry of program
+ * _putchar - prints to stdout
+ * Return: 0 on success
+ * Author - Nana Obeng
+ */
+
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
