2289c34992a2c922e81831aa4dd1ba0b78cb9eed
nkweku66 <nkweku66@gmail.com>
2023-09-17 16:48:30 -0700

    print nine times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Author - Nana Obeng
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar((a * b) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
