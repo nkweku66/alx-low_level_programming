b7f311f3a71a109e97beb4111180354ad5083192
nkweku66 <nkweku66@gmail.com>
2023-09-17 15:14:42 -0700

    cheks wether number is positive or negative and returns the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks and print the sign of a number
+ * @n: argument
+ *
+ * Return: 0, 1 or -1 upon success
+ * Author - Nana Obeng
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	else
+	{
+		return (0);
+		_putchar('0');
+	}
+}
