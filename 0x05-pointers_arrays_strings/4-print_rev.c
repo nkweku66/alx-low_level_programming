222725363491b325ec2abfa1000767a53ab53f33
nkweku66 <nkweku66@gmail.com>
2023-09-19 09:55:58 -0700

    print a string in the reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string
+ * Author - Nana Obeng
+ */
+
+void print_rev(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	print_rev(s + 1);
+	_putchar(*s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_rev(char *s);
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
