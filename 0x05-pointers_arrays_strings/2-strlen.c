d9c79c7e1ccea9b40463c08875277014fb1f2c6f
nkweku66 <nkweku66@gmail.com>
2023-09-19 07:24:50 -0700

    return the len of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - gets the length of a string
+ * @s: character
+ *
+ * Return: i
+ * Author - Nana Obeng
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
