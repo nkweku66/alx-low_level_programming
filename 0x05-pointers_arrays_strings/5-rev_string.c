77f2e84a08f50782ca024d104143c819c6fa7e92
nkweku66 <nkweku66@gmail.com>
2023-09-19 10:59:09 -0700

    reverses a string without a newline

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string argument
+ * Author - Nana Obeng
+ */
+
+void rev_string(char *s)
+{
+	char *lv = s;
+
+	while (*lv != '\0')
+	{
+		lv++;
+	}
+
+	lv--;
+
+	while (lv <= s)
+	{
+		_putchar(*s);
+		lv--;
+	}
+}
