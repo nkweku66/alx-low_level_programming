99efe9551260eb38fc8aabdb916dd6ed74ed8fb4
nkweku66 <nkweku66@gmail.com>
2023-09-20 13:31:44 -0700

    prints all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ * @n: number
+ *
+ * Author - Nana Obeng
+ */
+
+void print_to_98(int n)
+{
+	int a = 0, b = 0;
+	
+	while (n <= 98)
+	{
+		if (n > 9)
+		{
+			a = n / 10;
+			b = n % 10;
+
+			_putchar(a + '0');
+			_putchar(b + '0');
+		}
+		else
+		{
+			_putchar(n + '0');
+		}
+
+		if (a < 9 || b < 8)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
+	}
+}
