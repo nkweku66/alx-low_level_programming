fc0c8f898ac392fed1d2d076d20cdc7d9b437074
nkweku66 <nkweku66@gmail.com>
2023-09-19 21:50:13 -0700

    print tehe second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints a half a string
+ * @str: the string
+ * Author - Nana Obeng
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, n, m;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	n = (i - 1) ? 2;
+
+	for (m = n + 1; str[m] != '\0'; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar('\n');
+}
