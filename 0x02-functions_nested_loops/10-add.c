9856bb413fdc83210d62f9eb94023fe28bea350c
nkweku66 <nkweku66@gmail.com>
2023-09-20 07:03:43 -0700

    adds two numbers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @a: first param
+ * @b: second param
+ *
+ * Return: results
+ * Author - Nana Obeng
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return res;
+}
