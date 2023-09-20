f3c9b7698c37fe2ce4f1dac542ff9d78f06ec9d3
nkweku66 <nkweku66@gmail.com>
2023-09-20 13:35:23 -0700

    prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
 void print_to_98(int n)
 {
 	int a = 0, b = 0;
-	
+
 	while (n <= 98)
 	{
 		if (n > 9)
@@ -31,6 +31,7 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
+		_putchar('\n');
 		n++;
 	}
 }
