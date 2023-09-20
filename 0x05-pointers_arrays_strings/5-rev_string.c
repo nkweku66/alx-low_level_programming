870da661f09aa5be7b4cf90e216f8e1dc8b4dbc7
nkweku66 <nkweku66@gmail.com>
2023-09-19 23:04:05 -0700

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,7 +18,7 @@ void rev_string(char *s)
 	for (i = 0; i < len / 2; i++)
 	{
 		char temp = s[i];
-		s[i] = s[length -i - 1];
+		s[i] = s[len -i - 1];
 		s[len - i - 1] = temp;
 	}
 	_putchar('\n');
