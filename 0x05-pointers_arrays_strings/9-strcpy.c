6ab216381c059b4cd49888af7a736c2a8ec49926
nkweku66 <nkweku66@gmail.com>
2023-09-21 23:10:04 -0700

    copies string from one place to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string into another
+ * @dest: container to hold the copied string
+ * @src: the source string
+ *
+ * Return: dest
+ * Author - Nana Obeng
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return dest;
+}
