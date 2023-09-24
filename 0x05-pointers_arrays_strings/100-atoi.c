6049b54bdc0cc617d860c3abecb85d108dcbde8c
nkweku66 <nkweku66@gmail.com>
2023-09-24 15:19:42 -0700

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#define INT_MIN (-2147483647 - 1)
 /**
  * _atoi - converts a string into an integer
  * @s: the string
@@ -10,27 +10,33 @@
 
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int found_digit = 0;
-    int i;
+	int result = 0;
+	int sign = 1;
+	int found_digit = 0;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == '-')
-        {
-            sign *= -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-            found_digit = 1;
-        }
-        else if (found_digit)
-        {
-            break;
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			int digit = s[i] - '0';
 
-    return result * sign;
+			if (result > (INT_MAX - digit) / 10)
+			{
+				return INT_MIN;
+			}
+
+			result = result * 10 + (s[i] - '0');
+			found_digit = 1;
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+	}
+	return result * sign;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
