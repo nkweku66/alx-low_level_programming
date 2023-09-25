154ac5474eea4804bb20cdaf9ed544ccf4c4231c
nkweku66 <nkweku66@gmail.com>
2023-09-25 15:52:24 -0700

    feat: locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer
+ * @c: character
+ *
+ * Return: s
+ * Author - Nana Obeng
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return NULL;
+}
