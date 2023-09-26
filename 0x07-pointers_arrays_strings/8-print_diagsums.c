0bde4ef75dba43fdb6bcb42aa5a2d8d7743ab4e1
nkweku66 <nkweku66@gmail.com>
2023-09-26 15:55:15 -0700

    feat: sum up the diagonal of an array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - sums the diagonal of a matrix
+ * @a: first array
+ * @b: second array
+ *
+ * Author - Nana Obeng
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum = 0, sum1 = 0, i, j;
+
+	for (i = 0; i < size; ++i)
+	{
+		sum += a[i * size + i];
+
+		sum1 += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d", sum, sum1);
+}
