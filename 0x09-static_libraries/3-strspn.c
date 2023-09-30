0d0bc55fbf2871e93476eacfe923dc75f7c4ddf4
nkweku66 <nkweku66@gmail.com>
2023-09-30 08:29:24 -0700

    fix: file update

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - strspn
+ * @s: pointer
+ * @accept: accept
+ *
+ * Return: n
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+			a++;
+		}
+		if (*a == '\0')
+		{
+			break;
+		}
+		s++;
+	}
+	return (count);
+}
