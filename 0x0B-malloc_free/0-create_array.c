d70ef096314e6478a93028fb0662d822ebd88cd6
nkweku66 <nkweku66@gmail.com>
2023-10-04 17:35:46 -0700

    feat: create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array
+ * @size: len of the array
+ * @c: initial character
+ *
+ * Return: Null on failure
+ * Author - Nana Obeng
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size != 0)
+	{
+		char *arr;
+
+		arr = (char *)malloc(sizeof(c) * size);
+
+		if (size != NULL)
+		{
+			unsigned int i;
+
+			for (i = 0, i < size; i++)
+			{
+				arr[i] = c;
+			}
+			return (arr);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,24 @@
+# Project Name
+
+The project looks at memory allocation.
+
+## Description
+
+ project does.
+
+## Usage
+
+.
+
+## Files
+
+- `file1.py`: Description of file 1.
+- `file2.html`: Description of file 2.
+
+## Author
+
+Nana Obeng Kwaku Dennis
+
+## Contact
+
+nkweku66@gmail.com
