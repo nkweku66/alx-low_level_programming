cee38b2562684ca80a3ff7b16f7aad4fdb6e80b2
nkweku66 <nkweku66@gmail.com>
2023-10-06 04:31:31 -0700

    feat: make a duplicate of a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strdup - create a duplicate of a string
+ * @str: string
+ *
+ * Return: success
+ */
+
+char *_strdup(char *str)
+{
+	if (str != '\0')
+	{
+		char *ret =(char *)malloc(_strlen(str) + 1);
+
+		if (ret != NULL)
+		{
+			_strcpy(ret, str);
+		}
+		return (ret);
+		free(ret);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: res
+ */
+
+char _strcpy(char *dest, char *src)
+{
+	char *strg = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (strg);
+}
+
+/**
+ * _strlen - gets the len of a string
+ * @s: the pointer to string
+ *
+ * Return: result
+ */
+
+int _strlen(char *s)
+{
+	return (*s != '\0' ? 1 + _strlen(s + 1) : 0);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+int _strlen(char *s);
+char _strcpy(char *dest, char *src);
+char *_strdup(char *str);
 int _putchar(char c);
 char *create_arrays(unsigned int size, char c);
 #endif
