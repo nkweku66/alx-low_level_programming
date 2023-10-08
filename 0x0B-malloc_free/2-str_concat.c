3dc869050dc35b43ea7aa2457a2b29b7d0eec8a5
nkweku66 <nkweku66@gmail.com>
2023-10-08 10:10:31 -0700

    feat: join two strings together

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * *str_concat - to join string
+ * @s1: destination
+ * @s2: source
+ *
+ * Return: string
+ * Author - Nana Obeng
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t i = 0, j = 0;
+
+	if (s1 != NULL && s2 != NULL)
+	{
+		char *strg;
+
+		strg = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
+
+		while (s1[i] != '\0')
+		{
+			strg[i] = s1[i];
+			i++;
+		}
+
+		while (s2[j] != '\0')
+		{
+			strg[i] = s2[j];
+			i++;
+			j++;
+		}
+		strg[i] = '\0';
+		return (strg);
+		free(*strg);
+	}
+	return NULL;
+}
+
+int _strlen(char *s)
+{
+	return ((s != '\0') ? 1 + _strlen(s + 1) : 0);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 char _strcpy(char *dest, char *src);
 char *_strdup(char *str);
