c99d2746aa4d719dab29975526394bb0ecfffb5c
nkweku66 <nkweku66@gmail.com>
2023-11-08 04:20:07 -0800

    fix: betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * dog_t *new_dog - creates new dog
  * @name: name of dog
@@ -21,14 +21,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if(new_dog->name == NULL)
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
 	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if(new_dog->owner == NULL)
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
