9b1937a53ff2a9c23372361032cf559f21e04978
nkweku66 <nkweku66@gmail.com>
2023-11-09 23:03:11 -0800

    Betty: style fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	
+
 	if (array && cmp)
 	{
 		int i;
@@ -30,5 +30,4 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 	}
 	return (-1);
-}
-				
+}				
