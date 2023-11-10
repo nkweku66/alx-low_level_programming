bdd71f54e555f6a286696bea8ec3093c5f70b7c6
nkweku66 <nkweku66@gmail.com>
2023-11-09 22:58:53 -0800

    feat: find the index on an element in an array

---
+++ 0x0F-function_pointers/2-init_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -  find the index of an array element
+ * @array: is a pointer to an array
+ * @size: is the size of the array
+ * @cmp: is the function
+ *
+ * Return: results
+ * Author - Nana Obeng
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	
+	if (array && cmp)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
+				
