4974835bef2af3162564fac0f7020184c5d259b8
nkweku66 <nkweku66@gmail.com>
2023-11-16 14:09:57 -0800

    fix: incorrect macro

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,15 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		int sum, i;
 
+		sum = 0;
+
 		va_list args;
 
 		va_start(args, n);
 
 		for (i = 0; i < n; i++)
 		{
-			sum += va_args(ptr, int);
+			sum += va_arg(ptr, int);
 		}
 		return (sum);
 	}
