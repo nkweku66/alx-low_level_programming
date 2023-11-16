2b4f5849ccbdf923098d83b457e9341ee6a43db9
nkweku66 <nkweku66@gmail.com>
2023-11-16 13:58:47 -0800

    feat: add infiit numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums anan infinite number of arguments
+ * @n: number of arguments
+ *
+ * Return: results
+ * Author -- Nana Obeng
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n != 0)
+	{
+		int sum, i;
+
+		va_list args;
+
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			sum += va_args(ptr, int);
+		}
+		return (sum);
+	}
+	return (0);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef VARD_H
+#define VARD_H
+#include <stdio.h>
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+#endif
