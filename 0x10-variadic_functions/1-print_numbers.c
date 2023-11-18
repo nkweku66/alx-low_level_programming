dc7b93253fb883a1c8b9debb331f70a58aae5dfe
nkweku66 <nkweku66@gmail.com>
2023-11-18 12:12:27 -0800

    feat: print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: a separator
+ * @n: number of arguments
+ *
+ * Author - Nana Obeng
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print;
+
+	if (*seperator != NULL)
+	{
+		va_start(print, n);
+
+		int i, num;
+
+		for (i = 0; i < n; i++)
+		{
+			num = va_arg(print, int);
+			printf("%d", num);
+
+			if (i != (n - 1))
+			{
+				printf("%c ", *seperator);
+			}
+			printf("\n");
+		}
+		va_end(print);
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARD_H
 #define VARD_H
-
+#include <stdio.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n,...);
 #endif
