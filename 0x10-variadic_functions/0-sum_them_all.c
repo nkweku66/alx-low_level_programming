529893d55339450b116241b59ad267f1bc869809
nkweku66 <nkweku66@gmail.com>
2023-11-18 11:02:59 -0800

    fix: define macros

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
  * sum_them_all - sums anan infinite number of arguments
@@ -11,18 +10,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+
+	int sum;
+	unsigned int i;
+
 	if (n == 0)
 	{
 		return (0);
 	}
 	
-	int sum, i;
-	
 	sum = 0;
 
-	va_list args;
-
 	va_start(args, n);
+
+	sum = 0;
 
 	for (i = 0; i < n; i++)
 	{
