1c3af384637e092337436d1cfde03b45cde5b52c
nkweku66 <nkweku66@gmail.com>
2024-04-11 07:36:52 -0700

    fix: changes to operation of algrthm

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,42 @@
 
 int main(void)
 {
-	unsigned long a = 1, b = 2, next;
-	int count = 0;
+	int i;
+	unsigned long a = 0, b = 1, next;
+	unsigned long h1, h2, l1, l2;
+	unsigned long h, l;
 
-	printf("%lu, %lu, ", a, b);
-
-	while (count < 98)
+	for (i = 0; i < 92; i++)
 	{
 		next = a + b;
 		printf("%lu, ", next);
 		a = b;
 		b = next;
-		count++;
 	}
-	printf("%lu\n", next);
+	h1 = a / 10000000000;
+	h2 = b / 10000000000;
+	l1 = a % 10000000000;
+	l2 = b % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		h = h1 + h2;
+		l = l1 + l2;
+		if ( l1 + l2 > 9999999999)
+		{
+			h++;
+			l %= 10000000000;
+		}
+
+	printf("%lu%lu", h, l);
+	if (i != 98)
+		printf(", ");
+
+	h1 = h2;
+	h2 = h;
+	l1 = l2;
+	l2 = l;
+	}
+	printf("\n");
 	return (0);
 }
