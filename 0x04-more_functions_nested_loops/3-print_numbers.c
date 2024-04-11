29d7d056f2be68e219e5ea4132d84214836d2913
nkweku66 <nkweku66@gmail.com>
2024-04-11 10:25:47 -0700

    fix: add newline print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0-9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
