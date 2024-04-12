dc6f2df1f11cfc966c34023cb07d618402009e89
nkweku66 <nkweku66@gmail.com>
2024-04-12 15:40:40 -0700

    fix: change variables

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,18 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, a, b;
 
 	for (i = 0; i <= 14; i++)
 	{
+		a = i / 10;
+		b = i % 10;
+
 		if (i >= 10)
 		{
-			_putchar('0' + i / 10);
+			_putchar('0' + a);
 		}
-		_putchar('0' + i % 10);
+		_putchar('0' + b);
 	}
 	_putchar('\n');
 }
