eaa1ad4b9d94684741421c75ffd02acb5febb8b7
nkweku66 <nkweku66@gmail.com>
2024-04-12 15:25:14 -0700

    feat: prints more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints more number
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i <= 14; i++)
+	{
+		if (i >= 10)
+		{
+			_putchar('0' + i / 10);
+		}
+		_putchar('0' + i % 10);
+	}
+	_putchar('\n');
+}
