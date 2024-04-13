3a81c40c00a3ce86b7502ff2074c86994a09f028
nkweku66 <nkweku66@gmail.com>
2024-04-13 01:36:21 -0700

    fix: temp

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,17 @@
 
 void more_numbers(void)
 {
-	int i, j, b;
+	int i, j;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			b = j % 10;
-
 			if (j >= 10)
 			{
 				_putchar('1');
 			}
-			_putchar('0' + b);
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
