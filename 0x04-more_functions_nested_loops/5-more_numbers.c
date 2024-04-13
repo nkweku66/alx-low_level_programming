ad9caf9a39746819414ceea25e5f43332955acf9
nkweku66 <nkweku66@gmail.com>
2024-04-13 01:53:17 -0700

    fix: algo tweek

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, a, b;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			a = j / 10 + '0';
+			b = j % 10 + '0';
+
+			if (j > 9)
 			{
-				_putchar('1');
+				_putchar(a);
 			}
-			_putchar(j % 10 + '0');
+			_putchar(b);
 		}
 		_putchar('\n');
 	}
