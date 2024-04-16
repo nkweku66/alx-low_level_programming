9c639487d2101c61e21cefaf4eade67e854128ce
nkweku66 <nkweku66@gmail.com>
2024-04-16 11:32:16 -0700

    fix: modify algorithm

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,6 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	/*if (n <= 0)
-	{
-		_putchar('\n');
-	}*/
-
 	for (i = 1; i <= n; i++)
 	{
 		for (j = 1; j <= i; i++)
@@ -26,5 +21,5 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-	_putchar('\n');
+	/*_putchar('\n');*/
 }
