8b1a74b64844be11eaa60719722b85790f240845
nkweku66 <nkweku66@gmail.com>
2024-04-17 00:57:09 -0700

    fix: algo tweak

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints character in diagonal
+ * print_diagonal - prints in diagonal
  * @n: number of times to print
  *
  * Return: nothing
@@ -12,14 +12,18 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= i; i++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
-	/*_putchar('\n');*/
 }
