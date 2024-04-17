102f66e1375e3152b2b93ba75594890df8372063
nkweku66 <nkweku66@gmail.com>
2024-04-17 01:40:46 -0700

    feat: print character in square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,14 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n < 0)
+	if (size < 0)
 	{
 		_putchar('\n');
 	}
 
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j < n; j++)
+		for (j = 1; j < size; j++)
 		{
 			_putchar('#');
 		}
