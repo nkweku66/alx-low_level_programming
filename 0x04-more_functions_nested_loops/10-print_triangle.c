261d0c14ec4e3afe0cbdc96324b9f8919eee3672
nkweku66 <nkweku66@gmail.com>
2024-04-25 13:09:45 -0700

    feat: print the triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: noting
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		// Print the leading space
+		for (j = 1; j <= size - 1; j++)
+		{
+			_putchar(' ');
+		}
+
+		// print '#' character
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
